04fe896f2a5f6c8894d9e278dfdf0ec647018e03
ATHULKNAIR <77236185+ATHULKNAIR@users.noreply.github.com>
2024-07-19 00:37:22 +0530

    Create 6.ZigZagConversion.ts

---
+++ 6.ZigZagConversion.ts
@@ -0,0 +1,44 @@
+// The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+// P   A   H   N
+// A P L S I I G
+// Y   I   R
+// And then read line by line: "PAHNAPLSIIGYIR"
+
+// Write the code that will take a string and make this conversion given a number of rows:
+
+// string convert(string s, int numRows);
+ 
+
+// Example 1:
+
+// Input: s = "PAYPALISHIRING", numRows = 3
+// Output: "PAHNAPLSIIGYIR"
+// Example 2:
+
+// Input: s = "PAYPALISHIRING", numRows = 4
+// Output: "PINALSIGYAHRPI"
+// Explanation:
+// P     I    N
+// A   L S  I G
+// Y A   H R
+// P     I
+// Example 3:
+
+// Input: s = "A", numRows = 1
+// Output: "A"
+
+function convert(s: string, numRows: number): string {
+    if (numRows === 1) return s;
+    const array = Array(numRows).fill("");
+    let index = 0;
+    let isIncrement = true;
+    for (let i = 0; i < s.length; i++) {
+        if (index === numRows - 1) isIncrement = false;
+        if (index === 0) isIncrement = true;
+        array[index] = array[index] + s[i];
+        if (isIncrement) index++;
+        else index--;
+    }
+    return array.join("");
+};
